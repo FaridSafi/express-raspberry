997a84d372fc6568ef750ea238558339e7372559
Farid Safi <faridsafi@users.noreply.github.com>
2018-02-27 15:10:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to your Raspberry");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening to port 3000");
+});
