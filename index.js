9f9a2d9dd8eec576c537738abfdf1e5e704751a0
Farid Safi <faridsafi@users.noreply.github.com>
2018-02-27 15:16:29 +0100

    log when get

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log("Someone requested a page");
   res.send("Welcome to your Raspberry");
 });
 
